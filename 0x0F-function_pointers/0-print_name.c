85425aa62017398c0d460e051731e63fb461a017
Mohammed-Bahaj <medbahajm@gmail.com>
2023-11-09 08:26:56 -0800

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that print name
+ *
+ * @f: variable to hold the string name
+ * @name: name of th person
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
